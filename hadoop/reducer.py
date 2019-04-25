44a36b7a9dc34a78f8d5d319a06733b5e211dffe
StealthRecon <jeroen.van.dam@hva.nl>
2019-04-25 13:21:33 +0200

    Create reducer.py

---
+++ hadoop/reducer.py
@@ -0,0 +1,18 @@
+ #!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+sum = []
+
+for line in sys.stdin:
+	try:
+	    sum.append(line)
+	except ValueError:
+		continue
+
+sum = sorted(sum)
+sum = list(set(sum))
+
+for name in sum:
+	print (name)
