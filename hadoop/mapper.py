9f7eff62a520f0f92961b8aacddf50c4c6ede8ac
StealthRecon <jeroen.van.dam@hva.nl>
2019-05-14 12:54:56 +0200

    Update mapper.py
    hadoop jar /home/hadoop/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.1.2.jar 
    -file /home/hadoop/test/mapper.py  
    -mapper 'python mapper.py' 
    -file /home/hadoop/test/reducerV3.py 
    -reducer 'python reducerV3.py' 
    -input input/* 
    -output output/ 
    -cmdenv WORD_INPUT=Rey_AND_Skywalker

--- hadoop/mapper.py
+++ hadoop/mapper.py
@@ -1,23 +1,54 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
 
 import os
 import sys
 
-list = []
-
-file_type ="txt"
 search_str = os.environ["WORD_INPUT"]
+search_str = search_str.replace("_", " ")
+words = search_str.split()
+the_lines = []
+var = 0
+
+
+def createlist(search_word, the_lines):
+    list = []
+    search = search_word.lower()
+    for line in the_lines:
+        index = line.find(search)
+        if (index != -1):
+            filepath = os.environ["map_input_file"]
+            file_name = os.path.split(filepath)[-1]
+            if file_name not in list:
+                list.append(file_name)
+    return(list)
 
 for line in sys.stdin:
-    line = line.strip() #Remove white spaces in line of text
-    line = line.lower() #Convert line to lower case
-    index = line.find(search_str) #Search for string in line
-        if (index != -1): #If found:
-            #file_name = os.getenv('map_input_file')
-            filepath = os.environ["map_input_file"] #Get file path
-            file_name = os.path.split(filepath)[-1] #Get file name from path
-            if file_name not in list: #If the file isnt in the list already:
-                list.append(file_name) #Add file to list
-for name in list: 
-    print(list)
-	
+    line = line.strip()
+    line = line.lower()
+    the_lines.append(line)
+
+for search_word in words:
+    if search_word == "AND":
+        var = 1
+    elif search_word == "OR":
+        var = 2
+    elif search_word == "NOT":
+        var = 3
+    else:
+        if var == 1:
+            list_add = createlist(search_word, the_lines)
+            list_full = list(set(list_full) - set(list_add))
+            list_full = list_full + list_add
+        elif var == 2:
+            list_dif = createlist(search_word, the_lines)
+            list_one = list(set(list_full) - set(list_dif))
+            list_two = list(set(list_dif) - set(list_full))
+            list_full = list_one + list_two
+        elif var == 3:
+            list_sub = createlist(search_word, the_lines)
+            list_full = list(set(list_full) - set(list_sub))
+        else:
+            list_full = createlist(search_word, the_lines)
+
+for name in list_full:
+    print(name)
